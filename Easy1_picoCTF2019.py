32bdeb1b2f72c3acc6fe218dc3e81564827e5eb0
dalcio99 <72498508+dalcio99@users.noreply.github.com>
2022-08-04 15:09:54 +0200

    Add files via upload

---
+++ Easy1_picoCTF2019.py
@@ -0,0 +1,27 @@
+# picoCTF2019
+# Easy1
+# authors: Alex Fulton/Danny 
+
+cyph= "UFJKXQZQUNB"
+cyph_list = []
+
+key = "SOLVECRYPTO"
+key_list = []
+
+plain_list = []
+
+for c in cyph:
+   cyph_list.append(ord(c) - 65)
+
+for c in key:
+   key_list.append(ord(c) - 65)
+
+for i in range(0, len(cyph_list)):
+    plain_list.append((cyph_list[i] - key_list[i] ) % 26)
+
+plain = ""
+
+for i in range(0, len(cyph_list)):
+    plain = plain + chr(plain_list[i]+65)
+
+print(plain)
