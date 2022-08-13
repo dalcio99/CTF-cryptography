f240caa006b074ee457a599c715611088084a481
dalcio99 <72498508+dalcio99@users.noreply.github.com>
2022-08-13 21:16:47 +0200

    Update compress_and_attack_picoCTF2021.py

--- compress_and_attack_picoCTF2021.py
+++ compress_and_attack_picoCTF2021.py
@@ -1,6 +1,10 @@
 # Compress and attack
 # picoCTF 2021
 
+# The idea is that running the programme, it returns the length of the encrypted text (which is obtained summing
+# the flag and what you commit). The thing to notice is that committing some string which is contained into the 
+# flag, will need a shorter cyphertext (because of the compression).
+
 import string
 from pwn import *
 
